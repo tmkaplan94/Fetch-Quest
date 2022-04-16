68e55e696e1def6e2f5801732418a908b385638d
Jihigera <grantreed27@gmail.com>
2022-04-15 20:18:33 -0700

    Trying to combine Nat and Gavin's Scripts. Wasn't successful.

---
+++ Assets/_FetchQuest/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraNathaniel : MonoBehaviour
+{
+    public float sensitivityOfMouse;
+    [SerializeField]
+    Transform Player;
+    //Transform PlayerArms; 
+
+    float xAxisclasmp;
+    bool cursorLocked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //sensitivityOfMouse = 0.8f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateCamera();
+    }
+
+    void RotateCamera()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        float rotAmountX = mouseX * sensitivityOfMouse;
+        float rotAmountY = mouseY * sensitivityOfMouse;
+
+        xAxisclasmp -= rotAmountY;
+
+        //Vector3 rotArms = PlayerArms.transform.rotation.eulerAngles;
+        Vector3 rotPlayer = Player.transform.rotation.eulerAngles;
+
+        //rotPlayer.x -= rotAmountY;
+        rotPlayer.y += rotAmountX;
+        rotPlayer.z = 0;
+
+
+        /*if (xAxisclasmp > 90)
+        {
+            xAxisclasmp = 90;
+            rotPlayer.x = 90;
+        }
+        else if (xAxisclasmp < -90)
+        {
+            xAxisclasmp = -90;
+            rotPlayer.x = 270;
+        }*/
+
+        Player.rotation = Quaternion.Euler(rotPlayer);
+        //PlayerArms.rotation = Quaternion.Euler(rotArms);
+    }
+
+}
+
