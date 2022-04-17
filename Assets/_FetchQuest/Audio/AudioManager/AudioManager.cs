dea25be960f46358f65dc7dfb9b43a64af06660f
Jihigera <grantreed27@gmail.com>
2022-04-16 18:10:32 -0700

    merged main and adjusted playermovement script.

---
+++ Assets/_FetchQuest/Audio/AudioManager/AudioManager.cs
@@ -0,0 +1,155 @@
+﻿/*
+ * Author: Loc Trinh
+ * Contributors: Epitome
+ * Summary: Manages different audio clips throughout scenes.
+ *
+ * Description
+ * - To use Music: AudioManager.Instance.PlayMusic(<name>);
+ * - To use SFX: AudioManager.Instance.PlaySFX(<name>);
+ * - To use Cross Fade: AudioManager.Instance.PlayMusicWithCrossFade(<name>, float)
+ * - Music Names: ["Life_of_a_Pet.mp3"]
+ * - SFX Names: ["Crowd_Background.mp3", 
+                 "Cursor_Click_SFX.mp3", 
+                 "Cursor_Hover_SFX.mp3", 
+                 "Pick_Up_SFX.mp3", 
+                 "Score_Up_SFX.mp3"]
+ *
+ * Updates
+ * - N/A
+ */
+
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    #region Instance
+    private static AudioManager instance;
+    public static AudioManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<AudioManager>();
+                if (instance == null)
+                {
+                    instance = new GameObject("Spawned AudioManager", typeof(AudioManager)).GetComponent<AudioManager>();
+                }
+            }
+
+            return instance;
+        }
+        set
+        {
+            instance = value;
+        }
+    }
+    #endregion
+
+    #region Fields
+    public AudioSource[] musicSources;
+    private AudioSource currentMusic;
+    public AudioSource[] sfxSource;
+    private AudioSource currentSFX;
+    [SerializeField] private float musicVolume = 1.0f;
+    private bool firstMusicSourceIsActive;
+    #endregion
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        // Make sure to enable loop on music sources
+        currentMusic.loop = true;
+    }
+
+    public void PlayMusic(AudioClip musicClip)
+    {
+        foreach(AudioSource _source in musicSources)
+        {
+            if(_source.clip == musicClip)
+            {
+                currentMusic = _source;
+                _source.Play();
+                break;
+            }
+        }
+    }
+    // GET BACK TO THIS LATER. TOO TIRED RN [3:29AM]
+    /*
+    public void PlayMusicWithCrossFade(AudioClip musicClip, float transitionTime = 1.0f)
+    {
+        // Determine which source is active
+        AudioSource activeSource = (firstMusicSourceIsActive)? currentMusic;
+        AudioSource newSource = (firstMusicSourceIsActive) ? musicSource2 : musicSource;
+
+        // Swap the source
+        firstMusicSourceIsActive = !firstMusicSourceIsActive;
+
+        // Set the fields of the audio source, then start the coroutine to crossfade
+        newSource.clip = musicClip;
+        newSource.Play();
+        StartCoroutine(UpdateMusicWithCrossFade(activeSource, newSource, musicClip, transitionTime));
+    }
+    private IEnumerator UpdateMusicWithCrossFade(AudioSource original, AudioSource newSource, AudioClip music, float transitionTime)
+    {
+        // Make sure the source is active and playing
+        if (!original.isPlaying)
+            original.Play();
+
+        newSource.Stop();
+        newSource.clip = music;
+        newSource.Play();
+
+        float t = 0.0f;
+
+        for (t = 0.0f; t <= transitionTime; t += Time.deltaTime)
+        {
+            original.volume = (musicVolume - ((t / transitionTime) * musicVolume));
+            newSource.volume = (t / transitionTime) * musicVolume;
+            yield return null;
+        }
+
+        // Make sure we don't end up with a weird float value
+        original.volume = 0;
+        newSource.volume = musicVolume;
+
+        original.Stop();
+    }*/
+
+    public void PlaySFX(AudioClip clip)
+    {
+        foreach(AudioSource _source in sfxSource)
+        {
+            if(_source.clip == clip)
+            {
+                currentSFX = _source;
+                _source.Play();
+                break;
+            }
+        }
+    }
+    public void PlaySFX(AudioClip clip, float volume)
+    {
+        foreach(AudioSource _source in sfxSource)
+        {
+            if(_source.clip == clip)
+            {
+                _source.volume = volume;
+                currentSFX = _source;
+                _source.Play();
+                break;
+            }
+        }
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        currentMusic.volume = volume;
+    }
+    public void SetSFXVolume(float volume)
+    {
+        currentSFX.volume = volume; // prolly bug
+    }
+}
--- Assets/_FetchQuest/Scripts/Player/PlayerMovement.cs
+++ Assets/_FetchQuest/Scripts/Player/PlayerMovement.cs
@@ -14,53 +14,75 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private CharacterController _controller;
+    #region Serialized Private Variables
+    [SerializeField] private CharacterController _myCharacterController;
     [SerializeField] private Transform _cam;
 
     [SerializeField] private float _speed = 6;
+    [SerializeField] private float _sprintspeed = 10;
     [SerializeField] private float _gravity = -9.81f;
-    [SerializeField] private float _jumpHeight = 3;
-    private Vector3 _velocity;
-    private bool _isGrounded;
-
-    [SerializeField] private Transform _groundCheck;
-    [SerializeField] private float _groundDistance = 0.4f;
-    [SerializeField] private LayerMask _groundMask;
-
-    float _turnSmoothVelocity;
+    [SerializeField] private float _jumpStrength = 3;
     [SerializeField] private float _turnSmoothTime = 0.1f;
+    #endregion
+    #region Private Variables
+    private float _turnSmoothVelocity;
+    private Vector3 _moveDirection = Vector3.zero;
+    #endregion
 
     // Update is called once per frame
     void Update()
     {
-        //jump
-        _isGrounded = Physics.CheckSphere(_groundCheck.position, _groundDistance, _groundMask);
-        
-        if (_isGrounded && _velocity.y < 0)
-        {
-            _velocity.y = -2f;
-        }
-
-        if (Input.GetButtonDown("Jump") && _isGrounded)
-        {
-            _velocity.y = Mathf.Sqrt(_jumpHeight * -2 * _gravity);
-        }
-        //gravity
-        _velocity.y += _gravity * Time.deltaTime;
-        _controller.Move(_velocity * Time.deltaTime);
-        //walk
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
-        if (direction.magnitude >= 0.1f)
-        {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + _cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
-
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            _controller.Move(moveDir.normalized * _speed * Time.deltaTime);
-        }
+        Move();
     }
+
+    private void Move()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveZ = Input.GetAxisRaw("Vertical");
+
+        float movementSpeed = _speed;
+        
+        
+        if (_myCharacterController.isGrounded)
+        {
+            //using temp variable for finding angle of rotation.
+            Vector3 moveDirection = new Vector3(moveX, 0f, moveZ).normalized;
+            //setting _moveDirection to moveDirection to zero it if there is no input.
+            _moveDirection = moveDirection;
+            //checking to see if we need to actually move before we call controller.Move()
+            if (moveDirection.magnitude >= 0.1f)
+            {
+                //Getting angle to rotate too based off of player input moveDirection and camera position.
+                float targetAngle = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg + _cam.eulerAngles.y;
+                //smoothing player rotation.
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, _turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                //setting vector direction for player movement based on calculated angle.
+                _moveDirection  = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                //normalizing direction vector so we can use speed/sprint speed.
+                _moveDirection.Normalize();
+                //changing the speed if player is pressing shift.
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    movementSpeed = _sprintspeed;
+                }
+                //applying speed to direction vector.
+                _moveDirection *= movementSpeed;
+            }
+
+        //jump / Gravity
+        
+            if (Input.GetKey(KeyCode.Space))
+            {
+                _moveDirection.y += _jumpStrength;
+            }
+        }
+        else
+        {
+            _moveDirection.y -= _gravity;
+        }
+        //Applying actual movement forward for player.
+        _myCharacterController.Move(_moveDirection * Time.deltaTime);
+    } 
 }
