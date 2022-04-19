b2aba32564fa2b3e46d334a1614ae7e6d9fcbc16
Tyler Kaplan <tmkaplan94@gmail.com>
2022-04-19 10:51:28 -0700

    Made RotationManager, deleted old scripts

---
+++ Assets/_FetchQuest/Scripts/RotationManager.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class RotationManager : MonoBehaviour
+{
+    #region Private Serialized Fields
+    
+    
+    
+    #endregion
+
+
+    #region Private Fields
+    
+    
+    
+    #endregion
+
+
+    #region Properties
+    
+    
+    
+    #endregion
+
+
+    #region MonoBehavior Callbacks
+    
+    
+    
+    #endregion
+
+
+    #region Public Methods
+    
+    
+    
+    #endregion
+
+
+    #region Private Methods
+    
+    
+    
+    #endregion
+}
