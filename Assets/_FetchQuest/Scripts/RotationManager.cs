49aab1baad48e2e1dcc734934bbdedc08f72836d
Tyler Kaplan <tmkaplan94@gmail.com>
2022-04-19 14:07:53 -0700

    Rotation needs implementation

--- Assets/_FetchQuest/Scripts/RotationManager.cs
+++ Assets/_FetchQuest/Scripts/RotationManager.cs
@@ -1,34 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class RotationManager : MonoBehaviour
 {
     #region Private Serialized Fields
-
-    [SerializeField] private Transform center;
-    [SerializeField] private GameObject dogPositions;
-    [SerializeField] private Transform selectedLookAt;
+    
+    [SerializeField] private GameObject dogList;
+    [SerializeField] private Transform lookAtCamera;
+    [SerializeField] private Transform lookAtTarget;
     
     #endregion
 
 
     #region Private Fields
     
-    
-    
-    #endregion
+    private Vector3[] _dogPositions;
+    private GameObject[] _dogs;
+    private int _currentDogIndex;
+    private GameObject _currentDog;
 
-
-    #region Properties
-    
-    
-    
     #endregion
 
 
     #region MonoBehavior Callbacks
-    
-    
-    
+
+    private void Awake()
+    {
+        // initialize dogs, their positions, and set the current dog
+        InitializeDogsAndPositions();
+        _currentDogIndex = 0;
+        _currentDog = _dogs[_currentDogIndex];
+    }
+
+    private void Start()
+    {
+        // have the first dog look at target point on camera
+        //_currentDog.transform.LookAt(lookAtTarget);
+    }
+
     #endregion
 
 
@@ -36,12 +47,40 @@ public class RotationManager : MonoBehaviour
 
     public void RotateLeft()
     {
+        // rotate dog wheel left
+
+        // reset what current dog looks at
+        //_currentDog.transform.LookAt(lookAtCamera);
         
+        // change current dog
+        _currentDogIndex -= 1;
+        if (_currentDogIndex < 0)
+        {
+            _currentDogIndex = dogList.transform.childCount - 1;
+        }
+        _currentDog = _dogs[_currentDogIndex];
+        
+        // update new current dog's look at
+        //_currentDog.transform.LookAt(lookAtTarget);
     }
 
     public void RotateRight()
     {
+        // rotate dog wheel right
+
+        // reset what current dog looks at
+        //_currentDog.transform.LookAt(lookAtCamera);
         
+        // change current dog
+        _currentDogIndex += 1;
+        if (_currentDogIndex >= dogList.transform.childCount)
+        {
+            _currentDogIndex = 0;
+        }
+        _currentDog = _dogs[_currentDogIndex];
+        
+        // update new current dog's look at
+        //_currentDog.transform.LookAt(lookAtTarget);
     }
 
     #endregion
@@ -49,7 +88,20 @@ public class RotationManager : MonoBehaviour
 
     #region Private Methods
 
+    private void InitializeDogsAndPositions()
+    {
+        // get number of dogs to select and make an vector3 array
+        int size = dogList.transform.childCount;
+        _dogPositions = new Vector3[size];
+        _dogs = new GameObject[size];
 
+        // for each dog, get/store each dog and its position
+        for (int i = 0; i < size; i++)
+        {
+            _dogs[i] = dogList.transform.GetChild(i).GetChild(0).gameObject;
+            _dogPositions[i] = dogList.transform.GetChild(i).position;
+        }
+    }
 
     #endregion
 }
