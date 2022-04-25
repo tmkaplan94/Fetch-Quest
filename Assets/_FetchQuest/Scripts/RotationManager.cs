afaa25919a89ce74e70ff2992125eaa1151e04fd
juvaha <aportil2@uci.edu>
2022-04-25 00:50:20 -0700

    Huge update: rotation functionality added

--- Assets/_FetchQuest/Scripts/RotationManager.cs
+++ Assets/_FetchQuest/Scripts/RotationManager.cs
@@ -17,9 +17,12 @@ public class RotationManager : MonoBehaviour
     #region Private Fields
     
     private Vector3[] _dogPositions;
+    private Vector3[] _dogRotations;
     private GameObject[] _dogs;
     private int _currentDogIndex;
     private GameObject _currentDog;
+    private GameObject _previousDog;
+   
 
     #endregion
 
@@ -40,19 +43,32 @@ public class RotationManager : MonoBehaviour
         //_currentDog.transform.LookAt(lookAtTarget);
     }
 
-    #endregion
+    void Update(){
 
+        //rotates the current dog in the selected position
+        _currentDog.transform.Rotate(new Vector3(0, 50, 0) * Time.deltaTime);
+
+    }
+
+    #endregion
 
     #region Public Methods
 
     public void RotateLeft()
     {
+
         // rotate dog wheel left
         //Debug.Log("RotateLeft()");
-        dogList.transform.RotateAround(transform.position, Vector3.up, -30);
-
+        //dogList.transform.Rotate(new Vector3(0, -30, 0));
+        foreach(Transform child in dogList.transform){
+            child.gameObject.transform.RotateAround(dogList.transform.position, new Vector3(0,-1,0), 30);
+        }
         // reset what current dog looks at
         //_currentDog.transform.LookAt(lookAtCamera);
+        
+        //get previous dog to reset their rotate value
+        _previousDog = _currentDog;
+        correctOrientationForLeftRotation(_previousDog);
         
         // change current dog
         _currentDogIndex -= 1;
@@ -60,21 +76,38 @@ public class RotationManager : MonoBehaviour
         {
             _currentDogIndex = dogList.transform.childCount - 1;
         }
+
         _currentDog = _dogs[_currentDogIndex];
+
+        //reset rotation data 
+        
 
         // update new current dog's look at
         //_currentDog.transform.LookAt(lookAtTarget);
+
+;
     }
 
     public void RotateRight()
     {
         // rotate dog wheel right
-        //Debug.Log("RotateRight()");
-        dogList.transform.RotateAround(transform.position, Vector3.up, 30);
+
+        //children rotate with parent
+        //dogList.transform.Rotate(new Vector3(0, 30, 0));
+
+        //children rotate around parent 
+        foreach(Transform child in dogList.transform){
+            child.gameObject.transform.RotateAround(dogList.transform.position, new Vector3(0,1,0), 30);
+        }
 
         // reset what current dog looks at
         //_currentDog.transform.LookAt(lookAtCamera);
         
+        //get previous dog to reset their rotate value
+        //currentDog has not changed yet
+        _previousDog = _currentDog;
+        correctOrientationForRightRotation(_previousDog);
+
         // change current dog
         _currentDogIndex += 1;
         if (_currentDogIndex > dogList.transform.childCount - 1)
@@ -85,6 +118,7 @@ public class RotationManager : MonoBehaviour
 
         // update new current dog's look at
         //_currentDog.transform.LookAt(lookAtTarget);
+
     }
 
     #endregion
@@ -92,19 +126,32 @@ public class RotationManager : MonoBehaviour
 
     #region Private Methods
 
+
+    //For right rotates, previous dog needs to be set to a rotateY of 30 to maintain correct orientation
+    private void correctOrientationForRightRotation(GameObject previousDog){
+        previousDog.transform.eulerAngles = new Vector3(0, 30, 0);
+    }
+
+    //For left rotates, previous dog needs to be set to a rotateY of -30 to maintain correct orientation
+    private void correctOrientationForLeftRotation(GameObject previousDog){
+        previousDog.transform.eulerAngles = new Vector3(0, -30, 0);
+    }
+
     private void InitializeDogsAndPositions()
     {
         // get number of dogs to select and make an vector3 array
         int size = dogList.transform.childCount;
         _dogPositions = new Vector3[size];
+        _dogRotations = new Vector3[size];
         _dogs = new GameObject[size];
         
         // for each dog, get/store each dog and its position
         for (int i = 0; i < size; i++)
         {
-            _dogs[i] = dogList.transform.GetChild(i).GetChild(0).gameObject;
+            _dogs[i] = dogList.transform.GetChild(i).gameObject;
             _dogPositions[i] = dogList.transform.GetChild(i).position;
         }
+
     }
 
     #endregion
