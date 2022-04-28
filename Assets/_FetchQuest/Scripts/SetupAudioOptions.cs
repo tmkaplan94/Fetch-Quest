ce643a364aa818f141517e98e9fa943002a746a6
Jihigera <grantreed27@gmail.com>
2022-04-27 19:50:48 -0700

    addin audio options to pause menu

---
+++ Assets/_FetchQuest/Scripts/SetupAudioOptions.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetupAudioOptions : MonoBehaviour
+{
+    [SerializeField] private GameObject masterSlider;
+    [SerializeField] private GameObject musicSlider;
+    [SerializeField] private GameObject SFXSlider;
+    [SerializeField] private GameObject muteSFX;
+    [SerializeField] private GameObject muteMaster;
+    [SerializeField] private GameObject muteMusic;
+    void Start()
+    {
+        
+    }
+
+}
