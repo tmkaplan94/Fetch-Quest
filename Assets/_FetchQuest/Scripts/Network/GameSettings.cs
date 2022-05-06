f084ac4b1c41de4cd82e569af3a64646842be4af
Tyler Kaplan <tmkaplan94@gmail.com>
2022-05-06 12:19:37 -0700

    Created ScriptableObjects for network stuff

--- Assets/_FetchQuest/Scripts/Network/GameSettings.cs
+++ Assets/_FetchQuest/Scripts/Network/GameSettings.cs
@@ -1,7 +1,20 @@
 ﻿using UnityEngine;
 
 [CreateAssetMenu(fileName = "GameSettings", menuName = "ScriptableObject/GameSettings")]
-public class GameSettings : ScriptableObjectSingleton<GameSettings>
+public class GameSettings : ScriptableObject
 {
-        
+    #region Private Serialized Fields
+
+    [SerializeField] private string gameVersion = "1";
+    [SerializeField] private string nickName = "Doggo";
+
+    #endregion
+
+
+    #region Properties
+
+    public string GameVersion => gameVersion;
+    public string NickName => nickName;
+
+    #endregion
 }
--- Assets/_FetchQuest/Scripts/Network/NetworkManager.cs
+++ Assets/_FetchQuest/Scripts/Network/NetworkManager.cs
@@ -3,5 +3,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NetworkManager", menuName = "ScriptableObject/NetworkManager")]
 public class NetworkManager : ScriptableObjectSingleton<NetworkManager>
 {
+    #region Private Serialized Fields
 
+    [SerializeField] private GameSettings gameSettings;
+
+    #endregion
+
+
+    #region Properties
+
+    public static GameSettings GameSettings => Instance.gameSettings;
+
+    #endregion
 }
