90737ecc6941a4c9f9c40a53080f76e0f965d9b7
Alex <atpham6@uci.edu>
2022-05-22 06:18:53 -0700

    slowly trudging forward

--- Assets/_FetchQuest/Animation/PissSpecific/PissHandler.cs
+++ Assets/_FetchQuest/Animation/PissSpecific/PissHandler.cs
@@ -15,15 +15,54 @@ using UnityEngine;
 
 public class PissHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int pissThreshold = 45;
+    public Transform pissPoint = null;
+    public GameObject streamPrefab = null;
+
+    [SerializeField] private Animator _anime;
+    private bool isPissing = false;
+    //private Stream currentStream = null;
 
     // Update is called once per frame
     void Update()
     {
-        
+        bool pissCheck = CalcAngle() < pissThreshold;
+
+        if (isPissing != pissCheck)
+        {
+            isPissing = pissCheck;
+            if (isPissing)
+            {
+                StartPiss();
+            }
+            else
+            {
+                EndPiss();
+            }
+        }
     }
+
+    private void StartPiss()
+    {
+        print("start");
+        GameObject streamObject = Instantiate(streamPrefab, pissPoint.position, Quaternion.identity, transform);
+    }
+
+    private void EndPiss()
+    {
+
+    }
+
+    private float CalcAngle()
+    {
+        return transform.forward.y * Mathf.Rad2Deg;
+    }
+
+    /*
+    private Stream CreatePiss()
+    {
+        GameObject streamObject = Instantiate(streamPrefab, pissPoint.position, Quaternion.identity, transform);
+        return streamObject.GetComponent<Stream>();
+    }
+    */
 }
