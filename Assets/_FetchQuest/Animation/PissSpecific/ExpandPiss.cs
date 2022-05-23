25483a760df697f4ea85e1199e7bde6df5938870
Alex <atpham6@uci.edu>
2022-05-22 23:56:17 -0700

    testing new features

--- Assets/_FetchQuest/Animation/PissSpecific/ExpandPiss.cs
+++ Assets/_FetchQuest/Animation/PissSpecific/ExpandPiss.cs
@@ -4,35 +4,9 @@ using UnityEngine;
 
 public class ExpandPiss : MonoBehaviour
 {
-    private float _currentScale = InitScale;
-    private const float TargetScale = 1.1f;
-    private const float InitScale = 1f;
-    private const int FramesCount = 100;
-    private const float AnimationTimeSeconds = 2;
-    private float _deltaTime = AnimationTimeSeconds/FramesCount;
-    private float _dx = (TargetScale - InitScale)/FramesCount;
-    private bool _upScale = true;
-    // Start is called before the first frame update
-    void Start()
+    private float finalx = 1.3f, finaly = .7f, finalz = .3f;
+    private void Start()
     {
-        StartCoroutine(goBigger());
-    }
 
-    private IEnumerator goBigger()
-    {
-        while (true)
-        {
-            while (_upScale)
-            {
-                _currentScale += _dx;
-                if (_currentScale > TargetScale)
-                {
-                    _upScale = false;
-                    _currentScale = TargetScale;
-                }
-                transform.localScale = Vector3.one * _currentScale;
-                yield return new WaitForSeconds(_deltaTime);
-            }
-        }
     }
 }
