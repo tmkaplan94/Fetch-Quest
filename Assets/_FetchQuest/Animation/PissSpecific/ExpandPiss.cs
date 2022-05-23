5e473b737d47328982a55ed39c51721227fbb88d
Alex <atpham6@uci.edu>
2022-05-23 00:29:17 -0700

    it's fully expanded

--- Assets/_FetchQuest/Animation/PissSpecific/ExpandPiss.cs
+++ Assets/_FetchQuest/Animation/PissSpecific/ExpandPiss.cs
@@ -7,13 +7,15 @@ public class ExpandPiss : MonoBehaviour
     private float initx = .1f, inity = .07f, initz = .02f;
     private float finalx = 1.3f, finaly = .7f, finalz = .3f;
 
-    static float t = 0.0f;
+    static float tx = 0.0f, ty = 0.0f, tz = 0.0f;
 
     private void Update()
     {
         //transform.localScale = transform.localScale + new Vector3 (.1f * Time.deltaTime, .1f * Time.deltaTime, .1f * Time.deltaTime);
-        transform.localScale = new Vector3(Mathf.Lerp(initx, finalx, t), Mathf.Lerp(inity, finaly, t), Mathf.Lerp(initz, finalz, t));
+        transform.localScale = new Vector3(Mathf.Lerp(initx, finalx, tx), Mathf.Lerp(inity, finaly, ty), Mathf.Lerp(initz, finalz, tz));
 
-        t += 0.1f * Time.deltaTime;
+        tx += 0.1f * Time.deltaTime;
+        ty += 0.1f * Time.deltaTime;
+        tz += 0.05f * Time.deltaTime;
     }
 }
