2ae87f6956afb20c44b3c0fd38b4ffaa19ff3f99
Alex <atpham6@uci.edu>
2022-05-23 01:03:35 -0700

    just a little testing

--- Assets/_FetchQuest/Animation/PissSpecific/ExpandPiss.cs
+++ Assets/_FetchQuest/Animation/PissSpecific/ExpandPiss.cs
@@ -12,10 +12,12 @@ public class ExpandPiss : MonoBehaviour
     private void Update()
     {
         //transform.localScale = transform.localScale + new Vector3 (.1f * Time.deltaTime, .1f * Time.deltaTime, .1f * Time.deltaTime);
-        this.transform.localScale = new Vector3(Mathf.Lerp(initx, finalx, tx), Mathf.Lerp(inity, finaly, ty), Mathf.Lerp(initz, finalz, tz));
+        transform.localScale = new Vector3(Mathf.Lerp(initx, finalx, tx), Mathf.Lerp(inity, finaly, ty), Mathf.Lerp(initz, finalz, tz));
 
         tx += 0.1f * Time.deltaTime;
         ty += 0.1f * Time.deltaTime;
         tz += 0.05f * Time.deltaTime;
+
+        Debug.Log(transform.position);
     }
 }
