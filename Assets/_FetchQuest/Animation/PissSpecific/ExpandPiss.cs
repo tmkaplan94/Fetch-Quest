7c7d78663140b1b7f0c7d7a057e4b46494f56e35
Alex <atpham6@uci.edu>
2022-05-23 00:20:29 -0700

    piss will expand

--- Assets/_FetchQuest/Animation/PissSpecific/ExpandPiss.cs
+++ Assets/_FetchQuest/Animation/PissSpecific/ExpandPiss.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class ExpandPiss : MonoBehaviour
 {
+    private float initx = .1f, inity = .07f, initz = .02f;
     private float finalx = 1.3f, finaly = .7f, finalz = .3f;
-    private void Start()
-    {
 
+    static float t = 0.0f;
+
+    private void Update()
+    {
+        //transform.localScale = transform.localScale + new Vector3 (.1f * Time.deltaTime, .1f * Time.deltaTime, .1f * Time.deltaTime);
+        transform.localScale = new Vector3(Mathf.Lerp(initx, finalx, t), Mathf.Lerp(inity, finaly, t), Mathf.Lerp(initz, finalz, t));
+
+        t += 0.1f * Time.deltaTime;
     }
 }
