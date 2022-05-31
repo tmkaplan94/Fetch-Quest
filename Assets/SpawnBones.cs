bfeb64f9deac4994f23720e76f5a40812b15791f
Jihigera <grantreed27@gmail.com>
2022-05-31 01:47:30 -0700

    fixed bone weirdness i think

--- Assets/SpawnBones.cs
+++ Assets/SpawnBones.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Photon.Pun;
 public class SpawnBones : MonoBehaviour
 {
 
     [SerializeField] private Transform[] _spawnPoints;
     [SerializeField] private GameObject _bone;
+    private bool _isNetworked;
 
     #region Instance
     private static SpawnBones instance;
@@ -31,10 +32,25 @@ public class SpawnBones : MonoBehaviour
     }
     #endregion
 
+    private void Awake()
+    {
+        if (FindObjectOfType<NetworkManager>() == null)
+        {
+            _isNetworked = false;
+        }
+        else
+        {
+            _isNetworked = true;
+        }
+    }
+
     public void SpawnNewBone()
     {
         int i = Random.Range(0, _spawnPoints.Length);
-        Instantiate(_bone, _spawnPoints[i]);
+        if (!_isNetworked)
+            Instantiate(_bone, _spawnPoints[i]);
+        else
+            PhotonNetwork.Instantiate("Bone", _spawnPoints[i].position, Quaternion.identity);
     }
 
 }
