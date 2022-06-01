0f3c3e5ffc83a2b1283c8121e9281b37ea4c6e1e
Jihigera <grantreed27@gmail.com>
2022-05-31 18:19:21 -0700

    setting up shiz

--- Assets/Button1.cs
+++ Assets/Button1.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Button1 : MonoBehaviour
+public class Button1 : Interactable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] ZombieCloning cloner;
+    public  void Interact(GameObject interactor)
     {
-        
+        cloner.Clone();
     }
 }
